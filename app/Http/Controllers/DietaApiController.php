06c33e0df66cb196bcbf74ebdaf8860308a1176d
Alumno <ulsa@iMacD105.local>
2019-05-09 12:48:02 -0700

    ServicioEvidencia

---
+++ app/Http/Controllers/DietaApiController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Paciente;
+use App\Dieta;
+use App\ComidaDieta;
+use Carbon;
+
+class DietaApiController extends Controller
+{
+    public function dieta($fecha, $paciente) {
+        $dieta = 
+            Dieta::where('inicio_semana',$fecha)->
+                where('id_paciente',$paciente)->first();
+            //SELECT * FROM dietas 
+            //WHERE inicio_semana = 01-01-1900 
+            //AND id_paciente = 6
+
+            //if($dieta != NULL)
+        if ($dieta) {
+            $comidas =
+                ComidaDieta::where('id_dieta', $dieta->id)->
+                get(); 
+            $dieta['comidas'] =
+                $comidas;
+        }
+        return $dieta;
+    }
+
+    public function nueva_evidencia($dieta, $dia_semana, $tiempo_alimentacion) {
+        $comida = ComidaDieta::where('id_dieta', $dieta)
+        ->where('id_dia_semana', $dia_semana)
+        ->where('id_tiempo_alimentacion', $tiempo_alimentacion)
+        ->first();
+
+        if($comida != NULL) {
+            $comida->$evidencia = $request->input('evidencia');
+            $hoy = Carbon::now();
+            $comida->$tiempo_evidencia = $hoy->toDateTimeString();
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+//api
+Route::get('/dieta/{fecha}/{paciente}','DietaApiController@dieta')
+    ->name('api.dietas.dieta');
+
+Route::post('evidencia/{dieta}/{dia_semana}/{tiempo_alimentacion}', 'DietaApiController@nueva_evidencia')
+    ->name('api.dietas.evidencias.store');
